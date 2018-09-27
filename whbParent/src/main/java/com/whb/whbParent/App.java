d97f90e40359293fa4d6e2fe469ef4cec7a791d8
王华北 <wanghuabei@51tuodao.com>
2018-09-27 10:05:19 +0800

    添加項目

---
+++ whbParent/src/main/java/com/whb/whbParent/App.java
@@ -0,0 +1,13 @@
+package com.whb.whbParent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
