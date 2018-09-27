c342dfcb4c63200702c727934063934e0ba70f9a
王华北 <wanghuabei@51tuodao.com>
2018-09-27 13:49:51 +0800

    项目启动配置

---
+++ whbParent/whbTest/src/main/java/org/whb/test/WhbTestApplication.java
@@ -0,0 +1,15 @@
+package org.whb.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class WhbTestApplication {
+    public static void main( String[] args ){
+    	SpringApplication.run(WhbTestApplication.class, args);
+    }
+}
